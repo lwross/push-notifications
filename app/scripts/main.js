623bd0e20ee7aab16b18a778ffb45f98cab0366f
Lachlan Ross <lachlan.ross@salesforce.com>
2016-12-07 13:49:16 +1100

    initial changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -21,7 +21,7 @@
 
 'use strict';
 
-const applicationServerPublicKey = '<Your Public Key>';
+const applicationServerPublicKey = 'BJFCVdUuKURDiJ3FeeQalUEXYzmDpIbBM4udk7sRnRRAjOB6UefTCXhIxhABjNtkItdaDpn_nv6JHlOP7u8hqgE';
 
 const pushButton = document.querySelector('.js-push-btn');
 
@@ -42,3 +42,37 @@ function urlB64ToUint8Array(base64String) {
   }
   return outputArray;
 }
+
+function initialiseUI() {
+  // Set the initial subscription value
+  swRegistration.pushManager.getSubscription()
+  .then(function(subscription) {
+    isSubscribed = !(subscription === null);
+
+    if (isSubscribed) {
+      console.log('User IS subscribed.');
+    } else {
+      console.log('User is NOT subscribed.');
+    }
+
+    updateBtn();
+  });
+}
+
+function updateBtn() {
+  if (isSubscribed) {
+    pushButton.textContent = 'Disable Push Messaging';
+  } else {
+    pushButton.textContent = 'Enable Push Messaging';
+  }
+
+  pushButton.disabled = false;
+}
+
+navigator.serviceWorker.register('sw.js')
+.then(function(swReg) {
+  console.log('Service Worker is registered', swReg);
+
+  swRegistration = swReg;
+  initialiseUI();
+})
--- app/sw.js
+++ app/sw.js
@@ -20,3 +20,20 @@
 /* eslint-env browser, serviceworker, es6 */
 
 'use strict';
+
+if ('serviceWorker' in navigator && 'PushManager' in window) {
+  console.log('Service Worker and Push is supported');
+
+  navigator.serviceWorker.register('sw.js')
+  .then(function(swReg) {
+    console.log('Service Worker is registered', swReg);
+
+    swRegistration = swReg;
+  })
+  .catch(function(error) {
+    console.error('Service Worker Error', error);
+  });
+} else {
+  console.warn('Push messaging is not supported');
+  pushButton.textContent = 'Push Not Supported';
+}
